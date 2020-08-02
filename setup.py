7c49f13bb71aff79cd3850fcae29fa5d70d86134
JoeVirtual <jochen_privat@gmx.de>
2020-08-02 07:36:49 +0200

    tests: remove python 3.9 to test

--- setup.py
+++ setup.py
@@ -199,7 +199,6 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development',
         'Topic :: Software Development :: Embedded Systems',
         'Topic :: Software Development :: Testing',
